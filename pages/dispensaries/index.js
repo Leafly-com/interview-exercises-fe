c4602e55e996864e6a5c43ddfeb5de4b0fee3e73
Jack Jennings <jack.jennings@leafly.com>
2021-05-21 17:13:51 -0700

    feat(dispensaries): add helpful properties to document

--- pages/dispensaries/index.js
+++ pages/dispensaries/index.js
@@ -4,13 +4,23 @@ const fetcher = (url) => fetch(url).then((res) => res.json());
 
 export default function Dispensaries() {
   const { data, error } = useSwr("/api/dispensaries", fetcher);
+
   if (!data) return null;
+
   const { stores, currentPage, pageCount } = data;
-  console.log(data);
+
   return (
     <div>
       {stores.map((store) => (
-        <div>{store.name}</div>
+        <div>
+          {/*
+            {store.coverImage}
+            {store.name}
+            {store.hasPickupEnabled}
+            {store.distanceToUser}
+            {store.tagline}
+          */}
+        </div>
       ))}
     </div>
   );
